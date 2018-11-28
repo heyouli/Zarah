044ceb1a0156cc0b914ff52328571ddf4fd597a1
heyouli <julia@julia-hofmann.com>
2018-11-28 21:54:45 +0100

    fixed index image width, added mail form

--- js/work.js
+++ js/work.js
@@ -6,12 +6,6 @@ var works = [
     url: "Events_Christmasmarket.html"
   },
   {
-    date: "16.11.2018, 7pm until late 到很晚",
-    title: "Cheese Fondue Craze</br>奶酪火锅聚会",
-    pic: "img/Events/Cheesefondue.Party_Web.jpg",
-    url: "Events_Cheesefondue.party.html"
-  },
-  {
     date: "From 17.11. every night after 6pm</br>11月17日起，每晚6点开始",
     title: "Cheese Fondue</br>奶酪火锅",
     pic: "img/Events/Cheesefondue.reg_web.jpg",
@@ -22,12 +16,6 @@ var works = [
   title: "初見</br>The Atlas of Strangers",
   pic: "img/Events/Exhibition_Nov_Web.jpg",
   url: "Events_Exhibition.html"
-  },
-  {
-    date: "25.11.2018, 5pm",
-    title: "My Cookie for You</br>我为你制作圣诞饼干",
-    pic: "img/Events/Mycookie_web.jpg",
-    url: "Events_Mycookie.html"
   },
   {
     date: "Sundays 每周日 10am-3pm",
